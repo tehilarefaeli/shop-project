2d1e2e7d37335f208fa2c8fd568fb28b39b6dd72
Yehudit <yeuditm123@gmail.com>
2023-01-25 20:11:32 +0200

    multiplie card

--- shop.js
+++ shop.js
@@ -1,22 +1,8 @@
-function init(){
-    const tt=document.getElementsByClassName("card");
-   for( i=0 ; i<9 ;i++ ){
-    document.appendChild(tt)
-   }
-}
 function loadProducts(){
   debugger;
-  for(var i=0; i<=3; i++){
-    // var p=document.getElementsByClassName("card");
-    // document.body.append(p);
-    const node = document.getElementById("card").lastChild;
-   const clone = node.cloneNode(true);
-
-   document.body.append(clone);
-    // 
-    // body.appendChild(p);
-    // divsOfProducts.innerHTML+=document.getElementById("card");
-   // document.getElementById("divsOfProducts").innerHTML+=document.getElementById("card")
-    //  body.innerHTML+=document.getElementById("divsOfProducts").innerHTML+=document.getElementById("card");
-  }
+  for(var i=0; i<3; i++){
+        const node = document.getElementById("card");
+        const clone = node.cloneNode(true);
+        document.body.appendChild(clone);
+    }
 }
