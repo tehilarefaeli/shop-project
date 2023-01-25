b3f7d04670062634dad9a7cf67b6ada193456efe
Tehila Refaeli <tehila1728@gmail.com>
2023-01-25 20:56:30 +0200

    create table to image

--- shop.js
+++ shop.js
@@ -1,8 +1,20 @@
-function loadProducts(){
-  debugger;
-  for(var i=0; i<3; i++){
-        const node = document.getElementById("card");
-        const clone = node.cloneNode(true);
-        document.body.appendChild(clone);
+function loadProducts() {
+
+    var table = document.createElement("table");
+
+    for (var i = 0; i < 3; i++) {
+        var row = document.createElement("tr");
+        for (var j = 0; j < 3; j++) {
+            var cell = document.createElement("td");
+
+            const node = document.getElementById("card");
+            const clone = node.cloneNode(true);
+            cell.appendChild(clone);
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
     }
+    document.body.appendChild(table);
+
+
 }
